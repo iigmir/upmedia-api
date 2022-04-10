abca29f56d39c02c5af1d1f20133af3316c24875
iigmir <roc120j@gmail.com>
2022-04-10 23:59:51 +0800

    Refactor

--- routes/article.js
+++ routes/article.js
@@ -6,12 +6,43 @@ export default async(req, res) => {
     const { SerialNo } = req.params;
     const source = await ArticlePage( SerialNo );
     const document = parse( source.data );
-    // const link_source = [...document.querySelectorAll("a")].map( get_link );
+    const BuildContents = (dom) => {
+        // const { textContent } = dom;
+        const text = dom.textContent.trim();
+        // Image module
+        const images = [...dom.querySelectorAll("img")].map((dom) => {
+            const { src, alt } = dom.attributes;
+            return { src, alt };
+        });
+        // Link module
+        const links = [...dom.querySelectorAll("a")].map((its) => ({
+            href: its.attributes.href,
+            text: its.textContent
+        }));
+        // Build result
+        const result = {
+            text,
+            images,
+            links,
+        };
+        if (text === "") {
+            delete result.text;
+        }
+        if (images.length < 1) {
+            delete result.images;
+        }
+        if (links.length < 1) {
+            delete result.links;
+        }
+        return result;
+    };
+    const tags = [...document.querySelectorAll(".tag a")]
+        .map(GetLink)
+        .map(({ text, type }) => ({ text, id: type }))
+    ;
     const result = {
         meta: GetSource(source, req),
-        tags: [...document.querySelectorAll(".tag a")]
-            .map( GetLink )
-            .map( ({ text, type }) => ({ text, id: type }) ),
+        tags,
         info: {
             title: document.querySelector(".title h1").innerText.trim(),
             author: document.querySelector(".author a").innerText.trim(),
@@ -21,36 +52,9 @@ export default async(req, res) => {
             src: document.querySelector(".img img").attributes.src,
             alt: document.querySelector(".img img").attributes.alt
         },
-        contents: [...document.querySelectorAll(".editor > *:not(#inline_ad)")].map( (dom) => {
-                // const { textContent } = dom;
-                const text = dom.textContent.trim();
-                // Image module
-                const images = [...dom.querySelectorAll("img")].map( (dom) => {
-                    const { src, alt } = dom.attributes;
-                    return { src, alt };
-                });
-                // Link module
-                const links = [...dom.querySelectorAll("a")].map( ( its ) => ({
-                    href: its.attributes.href,
-                    text: its.textContent
-                }) );
-                // Build result
-                const result = {
-                    text,
-                    images,
-                    links,
-                };
-                if( text === "" ) {
-                    delete result.text;
-                }
-                if( images.length < 1 ) {
-                    delete result.images;
-                }
-                if( links.length < 1 ) {
-                    delete result.links;
-                }
-                return result;
-            }).filter( (item) => Object.keys(item).length > 0 )
+        contents: [...document.querySelectorAll(".editor > *:not(#inline_ad)")]
+            .map( BuildContents )
+            .filter( (item) => Object.keys(item).length > 0 ),
     };
     res.jsonp(result);
 }
