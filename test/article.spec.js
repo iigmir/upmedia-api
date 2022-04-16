7d780718fa9d5c7db8a0c7bf7c531dd4ca9ea1c3
iigmir <roc120j@gmail.com>
2022-04-17 05:01:32 +0800

    Add test

--- test/article.spec.js
+++ test/article.spec.js
@@ -6,8 +6,8 @@ const requestWithSupertest = supertest(server);
 describe( "/article", () => {
     it( "should throw error if no id given", async () => {
         const res = await requestWithSupertest.get("/article");
-        strictEqual(res.status, 200);
-        strictEqual(res.type, 'json');
-        strictEqual(res.body, "error");
+        strictEqual(res.status, 400);
+        strictEqual(res.type, 'application/json');
+        strictEqual("error" in res.body, true);
     })
 })
