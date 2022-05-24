cf40fe1f6d4722a72b962e834c8be18fb2423f68
iigmir <roc120j@gmail.com>
2022-05-24 10:04:21 +0800

    Rewrite tests

--- test/article.spec.js
+++ test/article.spec.js
@@ -3,28 +3,27 @@ import supertest from "supertest";
 import { strictEqual } from "assert";
 
 describe( "GET /article", async () => {
-    const request = supertest(server);
-    const noid_result = await request.get("/article");
-    const vaild_result = await request.get("/article/2");
-    it( "should get 400 if no id given", async () => {
+    it( "should get 400 if no id given", () => {
+        const noid_result = supertest(server).get("/article");
         strictEqual(noid_result.status, 400);
         strictEqual("error" in noid_result.body, true);
     });
     it( "should get 404 if invaild id given", async () => {
+        const noid_result = supertest(server).get("/article");
         strictEqual(noid_result.status, 400);
         strictEqual("error" in noid_result.body, true);
     });
-    it( "should get success if article given", async () => {
+    it( "should get success if article given", () => {
+        const vaild_result = supertest(server).get("/article/2");
         strictEqual(vaild_result.status, 200);
-        
     });
-    it( "should get article URL if article given", async () => {
+    it( "should get article URL if article given", (done) => {
+        const vaild_result = supertest(server).get("/article/2");
         const res = vaild_result.body;
         strictEqual(res.meta.url, "https://www.upmedia.mg/news_info.php?Type=24&SerialNo=2");
         strictEqual(res.info.title, "颱風尼伯特撲台　高市山區慢性病患者將撤離");
     });
-    // FIXME: request.close is not a function
-    afterEach( (done) => {
-        request.close(done);
+    after((done) => {
+        server.close(done);
     })
 })
